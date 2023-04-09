26b62063a9ec4c665ee3b198170ae19d0b7c9c75
Henrique Felipini <hfelipini@gmail.com>
2023-04-09 18:25:41 -0300

    Code 2023.04.09

--- 1_terraform_gcp/variables.tf
+++ 1_terraform_gcp/variables.tf
@@ -20,11 +20,11 @@ variable "storage_class" {
 variable "BQ_DATASET" {
   description = "BigQuery Dataset that raw data (from GCS) will be written to"
   type = string
-  default = "trips_data_all"
+  default = "de_project_dataset"
 }
 
 variable "TABLE_NAME" {
   description = "BigQuery Table"
   type  = string
-  default = "ny_trips"
+  default = "de_project"
 }
