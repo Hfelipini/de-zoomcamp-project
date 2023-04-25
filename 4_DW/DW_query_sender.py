8ebb1cf6daf4d274e3b12ab254261e279ba2193e
Henrique Felipini <hfelipini@gmail.com>
2023-04-25 16:28:50 -0300

    Code 2023.04.25

--- 4_DW/DW_query_sender.py
+++ 4_DW/DW_query_sender.py
@@ -8,6 +8,7 @@ query = '''
     SELECT
         DISTINCT(ID), *
     FROM `de-zoomcamp-project-hfelipini.de_project_dataset.python_test_partitioned`
+    --WHERE DateTime = "2023-04-25 15:12:00+00:00"
     ORDER BY Ticker ASC, DateTime DESC
 '''
 
