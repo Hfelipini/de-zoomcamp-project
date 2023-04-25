423deec2bc8c19268541ec3424fca7360ca301c8
Henrique Felipini <hfelipini@gmail.com>
2023-04-25 15:18:52 -0300

    Code 2023.04.25

--- 4_DW/DW_partition.py
+++ 4_DW/DW_partition.py
@@ -4,22 +4,13 @@ from google.oauth2 import service_account
 credentials = service_account.Credentials.from_service_account_file(filename="C:/Users/hfeli/OneDrive/Documents/Cursos/DataEngineering/Projects/de-zoomcamp-project-important-files/de-zoomcamp-project-hfelipini-a9d06ac71bcd.json",
                                                                     scopes=["https://www.googleapis.com/auth/cloud-platform"])
 """Create Partitioned table for optimization"""
-query = '''
-    SELECT
-        DISTINCT(ID), *
-    FROM `de-zoomcamp-project-hfelipini.de_project_dataset.python_test_partitioned`
-    ORDER BY Ticker ASC, DateTime DESC
-'''
 
-query_2 = '''
+query = '''
     CREATE OR REPLACE TABLE `de-zoomcamp-project-hfelipini.de_project_dataset.python_test_partitioned`
     PARTITION BY DATE(DateTime)
     CLUSTER BY Ticker AS (
       SELECT * FROM `de-zoomcamp-project-hfelipini.de_project_dataset.python_test`
     );
 '''
-#query_result2 = pd.read_gbq(credentials=credentials, query=query_2)
 
 query_result = pd.read_gbq(credentials=credentials, query=query)
-print(len(query_result))
-print(query_result.head(20))
